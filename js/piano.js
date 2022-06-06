9c32e5837a5b19470ef52ab0f9a456f822679820
Shubha Khadgi <87235573+shubhaaaaaaa@users.noreply.github.com>
2022-06-06 21:31:17 +0545

    piano

--- js/piano.js
+++ js/piano.js
@@ -1,21 +1,18 @@
-var number = document.querySelectorAll("button").length;
-for (var i = 0; i < number; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", function () {
-    var key = this.innerHTML;
-    playdrum(key);
-  });
-
-  document.querySelectorAll("button")[i].addEventListener("mouseover", function () {
-    var key = this.innerHTML;
-    changedrum(key,i);
-  });
-
-
-}
 document.addEventListener("keypress", function (event) {
   playdrum(event.key);
 });
 
+$("button").click(function () {
+  playkey($(this).attr("class").split(" ")[0]);
+});
+
+$("button").hover(function(){
+  original = $(this).html();
+  var mytxt = $(this).attr("class").split(" ")[0];
+  $(this).html(mytxt);
+}, function(){
+ $(this).html(original);
+})
 
 function playdrum(key) {
   switch (key) {
@@ -144,102 +141,127 @@ function playdrum(key) {
   }
 }
 
-function changedrum(key,i) {
+function playkey(key) {
   switch (key) {
-    case "q":
-  document.querySelectorAll("button")[i].innerHTML="C";
+
+    case "C":
+      var tom1 = new Audio("../sounds/piano/4/C.mp3");
+      tom1.play();
       break;
 
-    case "2":
-      document.querySelectorAll("button")[i].innerHTML="C#";
+    case "CS":
+      var tom2 = new Audio("../sounds/piano/4/CS.mp3");
+      tom2.play();
       break;
 
-    case "w":
-      document.querySelectorAll("button")[i].innerHTML="D";
+    case "D":
+      var tom3 = new Audio("../sounds/piano/4/D.mp3");
+      tom3.play();
       break;
 
-    case "3":
-      document.querySelectorAll("button")[i].innerHTML="D#";
+    case "DS":
+      var tom4 = new Audio("../sounds/piano/4/DS.mp3");
+      tom4.play();
       break;
 
-    case "e":
-      document.querySelectorAll("button")[i].innerHTML="E";
+    case "E":
+      var tom5 = new Audio("../sounds/piano/4/E.mp3");
+      tom5.play();
       break;
 
-    case "r":
-      document.querySelectorAll("button")[i].innerHTML="F";
+    case "F":
+      var tom6 = new Audio("../sounds/piano/4/F.mp3");
+      tom6.play();
       break;
 
-    case "5":
-      document.querySelectorAll("button")[i].innerHTML="F#";
+    case "FS":
+      var tom7 = new Audio("../sounds/piano/4/FS.mp3");
+      tom7.play();
       break;
 
-    case "t":
-      document.querySelectorAll("button")[i].innerHTML="G";
+    case "G":
+      var tom8 = new Audio("../sounds/piano/4/G.mp3");
+      tom8.play();
       break;
 
-    case "6":
-      document.querySelectorAll("button")[i].innerHTML="G#";
+    case "GS":
+      var tom9 = new Audio("../sounds/piano/4/GS.mp3");
+      tom9.play();
       break;
 
-    case "y":
-      document.querySelectorAll("button")[i].innerHTML="A";
+    case "A":
+      var tom10 = new Audio("../sounds/piano/4/A.mp3");
+      tom10.play();
       break;
 
-    case "7":
-      document.querySelectorAll("button")[i].innerHTML="A#";
+    case "AS":
+      var tom11 = new Audio("../sounds/piano/4/AS.mp3");
+      tom11.play();
       break;
 
-    case "u":
-      document.querySelectorAll("button")[i].innerHTML="B";
+    case "B":
+      var tom12 = new Audio("../sounds/piano/4/B.mp3");
+      tom12.play();
       break;
 
-    case "i":
-      document.querySelectorAll("button")[i].innerHTML="C";
+    case "C":
+      var tom13 = new Audio("../sounds/piano/5/C.mp3");
+      tom13.play();
       break;
 
-    case "9":
-      document.querySelectorAll("button")[i].innerHTML="C#";
+    case "CNS":
+      var tom14 = new Audio("../sounds/piano/5/CS.mp3");
+      tom14.play();
       break;
 
-    case "o":
-      document.querySelectorAll("button")[i].innerHTML="D";
+    case "DN":
+      var tom15 = new Audio("../sounds/piano/5/D.mp3");
+      tom15.play();
       break;
 
-    case "0":
-      document.querySelectorAll("button")[i].innerHTML="D#";
+    case "DNS":
+      var tom16 = new Audio("../sounds/piano/5/DS.mp3");
+      tom16.play();
       break;
 
-    case "p":
-      document.querySelectorAll("button")[i].innerHTML="E";
+    case "EN":
+      var tom17 = new Audio("../sounds/piano/5/E.mp3");
+      tom17.play();
       break;
 
-    case "z":
-      document.querySelectorAll("button")[i].innerHTML="F";
+    case "FN":
+      var tom18 = new Audio("../sounds/piano/5/F.mp3");
+      tom18.play();
       break;
 
-    case "s":
-      document.querySelectorAll("button")[i].innerHTML="F#";
+    case "FNS":
+      var tom19 = new Audio("../sounds/piano/5/FS.mp3");
+      tom19.play();
       break;
 
-    case "x":
-      document.querySelectorAll("button")[i].innerHTML="G";
+    case "GN":
+      var tom20 = new Audio("../sounds/piano/5/G.mp3");
+      tom20.play();
       break;
 
-    case "d":
-      document.querySelectorAll("button")[i].innerHTML="G#";
+    case "GNS":
+      var tom21 = new Audio("../sounds/piano/5/GS.mp3");
+      tom21.play();
       break;
 
-    case "c":
-      document.querySelectorAll("button")[i].innerHTML="A";
+    case "AN":
+      var tom22 = new Audio("../sounds/piano/5/A.mp3");
+      tom22.play();
       break;
 
-    case "f":
-      document.querySelectorAll("button")[i].innerHTML="A#";
+    case "ANS":
+      var tom23 = new Audio("../sounds/piano/5/AS.mp3");
+      tom23.play();
       break;
 
-    case "v":
-      document.querySelectorAll("button")[i].innerHTML="B";
+    case "BN":
+      var tom24 = new Audio("../sounds/piano/5/B.mp3");
+      tom24.play();
       break;
 
     default:
